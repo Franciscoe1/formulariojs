9559ca556fc501f43c55f216f58996bef81f6786
cynthia <cynthiayolanda0206@hotmail.com>
2025-04-16 10:46:41 -0500

    v1

---
+++ javascrip/formulario.js
@@ -0,0 +1,9 @@
+/* procesar los datos del forulario mediante una funcion */
+function procesarDatos (){
+    //Obtener los datos del formulario
+    var nota1 = document.getElementById("nota1").value;
+    //Imprimir el dato obtenido del formulario
+    document.writeln("La nota ingresada es : "+nota1 + "<br>");
+    //ir al dato a un numero entero
+
+}
